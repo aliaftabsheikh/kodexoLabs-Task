da5e3da328bc137b607e644611a12ca217270206
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-05-21 12:41:51 +0500

    Fix some mistakes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from 'components/Navbar/Navbar';
 import Home from 'components/Pages/Home/Home';
+import { Routes, Route } from "react-router-dom";
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Home/>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>   
+       </div>
   )
 };
 
